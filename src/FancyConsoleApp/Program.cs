8939e90efa5c05dc62c3d4c8956bd86de1581072
Philip Wahlbom <philip@wahlbom.net>
2022-02-06 15:34:06 -0500

    Commit resync

--- src/FancyConsoleApp/Program.cs
+++ src/FancyConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace FancyConsoleApp
     {
         public static void Main(string[] args)
         {
+            var tax = TaxCalculator.Calculate(345.55m);
+            Console.WriteLine(tax);
+            Console.ReadLine();
         }
     }
 }
---
+++ src/MyClassLibrary/TaxCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class TaxCalculator
+    {
+        public static decimal Calculate(decimal price)
+        {
+            return price * 0.08m;
+        }
+    }
+}
